7915a605629e0c089624da9ea6f37ea3a3750e58
hoanghaiduong <hoanghaiduong1711@gmail.com>
2024-06-07 21:35:22 +0700

    final

---
+++ frontend/components/detail_order/detail_order.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable @next/next/no-img-element */
+'use client'
+
+import React, { useState, useMemo } from 'react'
+import { Button } from '@/components/ui/button'
+// import CardItemPay from './UI_component/card_item';
+import { Rate } from 'antd'
+
+const DetailOrder = (props) => {
+
+    const detailOrder = props.orderData.detailOrder;
+    return (
+        <div className="p-8 w-[100%]">
+          {detailOrder.id}
+        </div>
+    )
+}
+export default DetailOrder
